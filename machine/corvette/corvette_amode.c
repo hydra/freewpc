564be90148f8b4b6de24896d216729aa341c6c80
Dominic Clifton <me@dominicclifton.name>
2011-06-11 17:47:11 +0100

    Corvette: adding missing attract mode file from 57c303c5a006a00226b0

---
+++ machine/corvette/corvette_amode.c
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2011 by Dominic Clifton
+ *
+ * This file is part of FreeWPC.
+ *
+ * FreeWPC is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * FreeWPC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with FreeWPC; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+/* CALLSET_SECTION (corvette_amode, __machine2__) */
+#include <freewpc.h>
+
+// TODO have a bunch of corvette car images and each time the amode comes round to display a car use a different one.
+
+/**
+ * Scroll two cars, one after the other, from left to right.
+ *
+ * Note: Car graphics use a white background.
+ */
+void corvette_amode_scroll_two_cars( void ) {
+
+	dmd_alloc_pair();
+	dmd_clean_page_high();
+	dmd_clean_page_low();
+	dmd_invert_page (dmd_low_buffer);
+	dmd_invert_page (dmd_high_buffer);
+	dmd_show2();
+	task_sleep_sec(1);
+
+	dmd_alloc_pair();
+	frame_draw (IMG_CORVETTE_1979);
+	dmd_sched_transition (&trans_scroll_right);
+	dmd_show2 ();
+
+	dmd_alloc_pair();
+	dmd_clean_page_high();
+	dmd_clean_page_low();
+	dmd_invert_page (dmd_low_buffer);
+	dmd_invert_page (dmd_high_buffer);
+	dmd_sched_transition (&trans_scroll_right);
+	dmd_show2();
+	task_sleep_sec(1);
+
+	dmd_alloc_pair();
+	frame_draw (IMG_CORVETTE_1986);
+	dmd_sched_transition (&trans_scroll_right);
+	dmd_show2 ();
+
+	dmd_alloc_pair();
+	dmd_clean_page_high();
+	dmd_clean_page_low();
+	dmd_invert_page (dmd_low_buffer);
+	dmd_invert_page (dmd_high_buffer);
+	dmd_sched_transition (&trans_scroll_right);
+	dmd_show2();
+	task_sleep_sec(1);
+
+
+}
+
+CALLSET_ENTRY (corvette_amode, amode_page)
+{
+	dbprintf("corvette_amode: amode_page - start\n");
+
+	corvette_amode_scroll_two_cars();
+
+	dbprintf("corvette_amode: amode_page - exit\n");
+}
+
+
