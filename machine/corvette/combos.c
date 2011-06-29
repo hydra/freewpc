e54cc253556b52a9597306de479d81406b7b1468
Dominic Clifton <me@dominicclifton.name>
2011-06-30 00:47:14 +0100

    Corvette: working combo detection code, tested on real machine
    Still work-in-progress, needs cleanup

--- machine/corvette/combos.c
+++ machine/corvette/combos.c
@@ -23,171 +23,6 @@
 #ifdef CONFIG_COMBOS
 
 //
-// TODO from mach-combos.c
-//
-
-static combo_step_t cstp_left_outer_loop_entry = {
-	.flags = CSTP_NO_FLAGS,
-	.switches = 1,
-	.switch_list = {
-		{ SW_LEFT_OUTER_LOOP, 0 }
-	}
-};
-
-static combo_step_t cstp_left_outer_loop_exit = {
-	.flags = CSTP_NO_FLAGS,
-	.switches = 4,
-	.switch_list = {
-		{ SW_RIGHT_OUTER_LOOP, TIME_1S },
-		{ SW_LEFT_ROLLOVER,    TIME_3S },
-		{ SW_MIDDLE_ROLLOVER,  TIME_3S },
-		{ SW_RIGHT_ROLLOVER,   TIME_3S }
-	}
-};
-
-
-static combo_step_t cstp_right_outer_loop_entry = {
-	.flags = CSTP_NO_FLAGS,
-	.switches = 1,
-	.switch_list = {
-		{ SW_RIGHT_OUTER_LOOP, 0 }
-	}
-};
-
-static combo_step_t cstp_right_outer_loop_exit = {
-	.flags = CSTP_NO_FLAGS,
-	.switches = 4,
-	.switch_list = {
-		{ SW_LEFT_OUTER_LOOP, TIME_1S },
-		{ SW_LEFT_ROLLOVER,   TIME_3S },
-		{ SW_MIDDLE_ROLLOVER, TIME_3S },
-		{ SW_RIGHT_ROLLOVER,  TIME_3S }
-	}
-};
-
-static combo_step_t cstp_wildcard_5sec = {
-	.flags = CSTP_WILDCARD,
-	.switches = 0,
-	.time_allowed = TIME_5S
-};
-
-extern void callset_lr_rl_combo_shot(void);
-static combo_def_t lr_rl_combo = {
-	.fn = callset_lr_rl_combo_shot,
-	.name = "LR RL",
-	.steps = 5,
-	.step_list = {
-		&cstp_left_outer_loop_entry,
-		&cstp_left_outer_loop_exit,
-		&cstp_wildcard_5sec,
-		&cstp_right_outer_loop_entry,
-		&cstp_right_outer_loop_exit
-	}
-};
-
-extern void callset_rl_lr_combo_shot(void);
-static combo_def_t rl_lr_combo = {
-	.fn = callset_rl_lr_combo_shot,
-	.name = "RL LR",
-	.steps = 5,
-	.step_list = {
-		&cstp_right_outer_loop_entry,
-		&cstp_right_outer_loop_exit,
-		&cstp_wildcard_5sec,
-		&cstp_left_outer_loop_entry,
-		&cstp_left_outer_loop_exit
-	}
-};
-
-static combo_def_t rl_rl_combo = {
-	.fn = null_function,
-	.name = "RL RL",
-	.steps = 5,
-	.step_list = {
-		&cstp_right_outer_loop_entry,
-		&cstp_right_outer_loop_exit,
-		&cstp_wildcard_5sec,
-		&cstp_right_outer_loop_entry,
-		&cstp_right_outer_loop_exit
-	}
-};
-
-static combo_def_t lr_lr_combo = {
-	.fn = null_function,
-	.name = "LR LR",
-	.steps = 5,
-	.step_list = {
-		&cstp_left_outer_loop_entry,
-		&cstp_left_outer_loop_exit,
-		&cstp_wildcard_5sec,
-		&cstp_left_outer_loop_entry,
-		&cstp_left_outer_loop_exit
-	}
-};
-
-static combo_def_t ll_rr_combo = {
-	.fn = null_function,
-	.name = "LL RR",
-	.steps = 5,
-	.step_list = {
-		&cstp_left_outer_loop_entry,
-		&cstp_left_outer_loop_entry,
-		&cstp_wildcard_5sec,
-		&cstp_right_outer_loop_entry,
-		&cstp_right_outer_loop_entry
-	}
-};
-
-static combo_def_t rr_ll_combo = {
-	.fn = null_function,
-	.name = "RR LL",
-	.steps = 5,
-	.step_list = {
-		&cstp_right_outer_loop_entry,
-		&cstp_right_outer_loop_entry,
-		&cstp_wildcard_5sec,
-		&cstp_left_outer_loop_entry,
-		&cstp_left_outer_loop_entry
-	}
-};
-
-#define COMBO_COUNT 6
-
-#define LR_RL_COMBO_ID 0
-#define RL_LR_COMBO_ID 1
-#define RL_RL_COMBO_ID 2
-#define LR_LR_COMBO_ID 3
-#define LL_RR_COMBO_ID 4
-#define RR_LL_COMBO_ID 5
-
-combo_def_t *machine_combos[COMBO_COUNT] = {
-	&lr_rl_combo,
-	&rl_lr_combo,
-	&rl_rl_combo,
-	&lr_lr_combo,
-	&ll_rr_combo,
-	&rr_ll_combo
-};
-
-U8 machine_combos_count;
-U8 current_step_markers[COMBO_COUNT]; // 1-based index
-U16 step_time_list[COMBO_COUNT]; // the system timer at the time the last-match step that counted
-U16 step_time_allowed_list[COMBO_COUNT]; // the time that was allowed by the last-hit switch or step that counted
-
-void combo_reset_current_step_markers(void) {
-	last_matched_combo = 0;
-	memset(current_step_markers, 0x00, sizeof(current_step_markers));
-	memset(step_time_list, 0x00, sizeof(step_time_list));
-	memset(step_time_allowed_list, 0x00, sizeof(step_time_allowed_list));
-}
-
-CALLSET_ENTRY(machine_combos, init_complete, start_ball) {
-	machine_combos_count = COMBO_COUNT;
-	combo_reset_current_step_markers();
-}
-
-
-//
 // combos.c game code follows
 //
 
@@ -199,7 +34,7 @@ void combo_name_deff (void)
 
 CALLSET_ENTRY (combo, rl_lr_combo_shot)
 {
-	dbprintf("rl_lr_combo handler invoked\n");
+	//dbprintf("rl_lr_combo handler invoked\n");
 	// TODO sounds, deff, scoring
 	deff_start(DEFF_COMBO_NAME); // XXX
 	sound_start (ST_SAMPLE, SND_EXPLOSION_01, SL_2S, PRI_GAME_QUICK2); // XXX
@@ -208,7 +43,7 @@ CALLSET_ENTRY (combo, rl_lr_combo_shot)
 
 CALLSET_ENTRY (combo, lr_rl_combo_shot)
 {
-	dbprintf("lr_rl_combo handler invoked\n");
+	//dbprintf("lr_rl_combo handler invoked\n");
 	// TODO sounds, deff, scoring
 	deff_start(DEFF_COMBO_NAME); // XXX
 	sound_start (ST_SAMPLE, SND_EXPLOSION_01, SL_2S, PRI_GAME_QUICK2); // XXX
