a357409b33b0b6c4144ea8e9edb86e9cb7fea83f
Dominic Clifton <me@dominicclifton.name>
2011-07-02 14:24:08 +0100

    Unittests: whilespace cleanup

--- unittests/testrunner.c
+++ unittests/testrunner.c
@@ -33,54 +33,48 @@ extern void io_init(void);
 //
 // create a test...
 //
-void test_hello_world( void )
-{
-        char *s = "hello world!";
-        assert_string_equal("hello world!", s);
-        assert_string_contains("hello", s);
-        assert_string_doesnt_contain("goodbye", s);
-        assert_string_ends_with("!", s);
-        assert_string_starts_with("hell", s);
+void test_hello_world(void) {
+	char *s = "hello world!";
+	assert_string_equal("hello world!", s);
+	assert_string_contains("hello", s);
+	assert_string_doesnt_contain("goodbye", s);
+	assert_string_ends_with("!", s);
+	assert_string_starts_with("hell", s);
 }
 
 //
 // put the test into a fixture...
 //
-void test_fixture_hello( void )
-{
-        test_fixture_start();
-        run_test(test_hello_world);
-        test_fixture_end();
+void test_fixture_hello(void) {
+	test_fixture_start();run_test(test_hello_world);
+	test_fixture_end();
 }
 
 //
 // put the fixture into a suite...
 //
-void all_tests( void )
-{
-        test_fixture_hello();
-        test_fixture_switches();
+void all_tests(void) {
+	test_fixture_hello();
+	test_fixture_switches();
 #ifdef CONFIG_COMBOS
-        test_fixture_combos();
+	test_fixture_combos();
 #endif
 }
-
 
 //
 // freewpc unit test helpers
 //
 
-void db_puts_unittest (const char *s) {
+void db_puts_unittest(const char *s) {
 	printf(s);
 }
 
 //
 // run the suite!
 //
-int main( int argc, char** argv )
-{
+int main(int argc, char** argv) {
 	db_puts = db_puts_unittest;
 	io_init();
 
-        return run_tests(all_tests);
+	return run_tests(all_tests);
 }
